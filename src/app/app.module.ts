f645d5f81d2b54602137c216dba31cf12251a6fb
decraemerdante <decraemerdante@gmail.com>
2021-06-07 12:03:24 +0200

    Module 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavbarComponent } from './nav/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [    
+    EventsAppComponent, EventsListComponent, EventThumbnailComponent, NavbarComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
---
+++ src/app/events-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  templateUrl: './events-app.component.html',
+  styleUrls: []
+})
+export class EventsAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
